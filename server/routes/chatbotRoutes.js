262ccab49ef4ad1150fab33b0d3b1a92eff7bd4f
swathi <swathi.s.3dartist@gmail.com>
2026-06-17 23:06:04 +0530

    created the structure

--- server/routes/chatbotRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { handleChatMessage } from '../controllers/chatbotController.js';
-
-const router = express.Router();
-
-router.post('/', handleChatMessage);
-
-export default router;
